5182388390a9b6f9667cf0a7a626f88b23f44797
Michael Palmer <lawlux@gmail.com>
2017-10-13 01:20:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = Piglatinizer.new
+    @pl_text = pl.to_pig_latin(params[:user:phrase])
+
+    erb :results
+  end
+
+  
 end
