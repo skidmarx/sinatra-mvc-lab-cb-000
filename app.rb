0fff8cc3985fe316b85f2934bc9db502ecea5089
Michael Palmer <lawlux@gmail.com>
2017-10-13 01:28:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    pl = Piglatinizer.new
+    pl = PigLatinizer.new
     @pl_text = pl.to_pig_latin(params[:user_phrase])
 
     erb :results
